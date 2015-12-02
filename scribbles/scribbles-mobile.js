9da9d9afd4d4eec41e7b18b5d497f5e2f22dfb27
Charles Haneberg <chaneberg13@georgefox.edu>
2015-12-02 00:54:26 -0800

    small changes to scribbles

--- scribbles/scribbles-mobile.js
+++ scribbles/scribbles-mobile.js
@@ -8,6 +8,14 @@ canvas.addEventListener('mousemove', penMove);
 canvas.addEventListener('mouseup', penUp);
 canvas.addEventListener('mouseleave', penLeave);
 
+canvas.addEventListener("touchstart", function(e) {
+  e.preventDefault();
+  penDown(e.touches[0]);
+});
+canvas.addEventListener("touchmove", function(e) penMove(e.touches[0]));
+canvas.addEventListener("touchend", function(e) penUp(e.touches[0]));
+canvas.addEventListener("touchleave", function(e) penLeave(e.touches[0]));
+
 function penDown(event) {
   if(event.button == 0) {
     mouseDown = true;
@@ -38,7 +46,9 @@ function penLeave(event) {
 }
 
 function getMouse(event) {
-  return {x: event.offsetX, y: event.offsetY};
+  var rect = canvas.getBoundingClientRect();
+  return {x: event.clientX - rect.left, y: event.clientY - rect.top};
+  //return {x: event.offsetX, y: event.offsetY};
 }
 
 function drawLine(point1, point2) {
