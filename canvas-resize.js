db2a44ed6d0d26f738ac1d76f696385bf8f457d3
Charles Haneberg <chaneberg13@georgefox.edu>
2015-12-02 00:11:31 -0800

    testing scribbles on mobile

--- canvas-resize.js
+++ canvas-resize.js
@@ -1,11 +1,12 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-window.onresize = function(event) {
+window.addEventListener('resize', canvasResize);
+canvasResize();
+
+function canvasResize(event) {
   var picture = ctx.getImageData(0, 0, canvas.width, canvas.height);
   canvas.width  = canvas.clientWidth;
   canvas.height = canvas.clientHeight;
   ctx.putImageData(picture, 0, 0);
 };
-
-window.onresize();
--- scribbles/scribbles.js
+++ scribbles/scribbles.js
@@ -3,37 +3,49 @@ var ctx = canvas.getContext('2d');
 var oldMouse;
 var mouseDown = false;
 
-canvas.onmousedown = function(event) {
+canvas.addEventListener('mousedown', penDown);
+canvas.addEventListener('mousemove', penMove);
+canvas.addEventListener('mouseup', penUp);
+canvas.addEventListener('mouseleave', penLeave);
+
+canvas.addEventListener("touchstart", function(e) penDown(e.touches[0]));
+canvas.addEventListener("touchmove", function(e) penMove(e.touches[0]));
+canvas.addEventListener("touchend", function(e) penUp(e.touches[0]));
+canvas.addEventListener("touchleave", function(e) penLeave(e.touches[0]));
+
+function penDown(event) {
   if(event.button == 0) {
     mouseDown = true;
     oldMouse = getMouse(event);
     drawLine(oldMouse, oldMouse);
   }
-};
+}
 
-canvas.onmousemove = function(event) {
+function penMove(event) {
   if(mouseDown) {
     var mouse = getMouse(event);
     drawLine(oldMouse, mouse);
     oldMouse = mouse;
   }
-};
+}
 
-canvas.onmouseup = function(event) {
+function penUp(event) {
   if(event.button == 0) {
     mouseDown = false;
   }
-};
+}
 
-canvas.onmouseleave = function(event) {
+function penLeave(event) {
   if(mouseDown) {
     drawLine(oldMouse, getMouse(event));
     mouseDown = false;
   }
-};
+}
 
 function getMouse(event) {
-  return {x: event.offsetX, y: event.offsetY};
+  var rect = canvas.getBoundingClientRect();
+  return {x: event.clientX - rect.left, y: event.clientY - rect.top};
+  //return {x: event.offsetX, y: event.offsetY};
 }
 
 function drawLine(point1, point2) {
