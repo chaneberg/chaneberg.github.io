76585de78106823e967cd26722eb1ee6e06ce6b1
Charles Haneberg <chaneberg13@georgefox.edu>
2015-12-03 01:20:18 -0800

    testing script

--- push.bat
+++
@@ -1,5 +0,0 @@
-set /p message="Enter commit message: "
-call git add -A
-call git commit -m "%message%"
-call git push
-pause
--- push.sh
+++ push.sh
@@ -1,11 +1,8 @@
-#!C:\Users\Charles\AppData\Local\GitHub\GitHub.appref-ms --open-shell
 set -x
 echo "Enter commit message: "
 read message
 git add -A
 git commit -m "$message"
 git push
-send "chaneberg\n"
-send "nh4j1a0x!\n"
 echo Press enter...
 read
