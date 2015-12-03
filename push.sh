dabb9e26dc893b45c2a54f51a2a3238f48523234
Charles Haneberg <chaneberg13@georgefox.edu>
2015-12-03 02:10:47 -0800

    testing script

--- push.sh
+++ push.sh
@@ -1,8 +1,12 @@
-set -x
-echo "Enter commit message: "
+echo -e "\033[0;31mEnter commit message: \033[0m"
 read message
+
+echo -e "\033[0;31mgit add -A\033[0m"
 git add -A
+echo -e "\033[0;31mgit commit -m \"$message\"\033[0m"
 git commit -m "$message"
+echo -e "\033[0;31mgit push\033[0m"
 git push
-echo Press enter to close...
+
+echo -e "\033[0;31mPress enter to close...\033[0m"
 read
